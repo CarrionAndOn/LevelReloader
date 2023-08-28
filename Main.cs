d545357caf63229b4235a04bab18b05fc4a2e403
CarrionAndOn <maya@carrionandon.me>
2023-08-27 18:41:40 -0700

    Initial commit

---
+++ Main.cs
@@ -0,0 +1,24 @@
+﻿using MelonLoader;
+using SLZ.Marrow.SceneStreaming;
+using UnityEngine;
+
+namespace LevelReloader
+{
+    public class Main : MelonMod
+    {
+        internal const string Name = "Level Reloader"; // Required
+        internal const string Description = "Reloads the level if you hit R."; // Required
+        internal const string Author = "CarrionAndOn"; // Required
+        internal const string Company = "Weather Electric"; // Set as null if blank
+        internal const string Version = "1.0.0"; // Required
+        internal const string DownloadLink = "null"; // Set as null if blank
+        
+        public override void OnUpdate()
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                SceneStreamer.Reload();
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using MelonLoader;
+
+[assembly: AssemblyTitle(LevelReloader.Main.Description)]
+[assembly: AssemblyDescription(LevelReloader.Main.Description)]
+[assembly: AssemblyCompany(LevelReloader.Main.Company)]
+[assembly: AssemblyProduct(LevelReloader.Main.Name)]
+[assembly: AssemblyCopyright("Developed by " + LevelReloader.Main.Author)]
+[assembly: AssemblyTrademark(LevelReloader.Main.Company)]
+[assembly: AssemblyVersion(LevelReloader.Main.Version)]
+[assembly: AssemblyFileVersion(LevelReloader.Main.Version)]
+[assembly:
+    MelonInfo(typeof(LevelReloader.Main), LevelReloader.Main.Name, LevelReloader.Main.Version,
+        LevelReloader.Main.Author, LevelReloader.Main.DownloadLink)]
+[assembly: MelonColor(System.ConsoleColor.White)]
+
+// Create and Setup a MelonGame Attribute to mark a Melon as Universal or Compatible with specific Games.
+// If no MelonGame Attribute is found or any of the Values for any MelonGame Attribute on the Melon is null or empty it will be assumed the Melon is Universal.
+// Values for MelonGame Attribute can be found in the Game's app.info file or printed at the top of every log directly beneath the Unity version.
+[assembly: MelonGame("Stress Level Zero", "BONELAB")]
